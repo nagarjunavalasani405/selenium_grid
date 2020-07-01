2a5c8b59d8b94f3d8e2da890a3bb7d271885fde3
valasani <nagarjuna.valasani405@gmail.com>
2020-07-01 18:59:37 +0530

    first commit

---
+++ selenium_grid/src/main/java/seleniumgrid/OpenBrowser.java
@@ -0,0 +1,32 @@
+package seleniumgrid;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenBrowser {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\newchrome\\chromedriver.exe");
+	    WebDriver driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.name("Submit")).click();
+		
+		System.out.println(driver.getTitle());
+		
+		driver.quit();
+		
+		
+	}
+
+}
